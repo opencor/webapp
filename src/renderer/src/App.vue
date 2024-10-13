30fbf9980b382e44e5c743021ca260ab5de8ef66
Alan Garny <agarny@hellix.com>
2024-10-13 15:45:39 +1300

    Reset All: converted it to a component.
    To make App.vue as "clean" as possible.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,30 +1,4 @@
-<script setup>
-import { useConfirm } from 'primevue/useconfirm'
-
-const confirm = useConfirm()
-
-if (window.electronAPI !== undefined) {
-  window.electronAPI.onResetAll(() => {
-    confirm.require({
-      header: 'OpenCOR',
-      icon: 'pi pi-question-circle',
-      message: 'You are about to reset all of your settings. Do you want to proceed?',
-      acceptProps: {
-        severity: 'danger'
-      },
-      rejectProps: {
-        severity: 'secondary',
-        outlined: true
-      },
-      accept: () => {
-        window.electronAPI.resetAll()
-      }
-    })
-  })
-}
-</script>
-
 <template>
   <Background />
-  <ConfirmDialog style="max-width: 75vw" />
+  <ResetAllDialog />
 </template>
---
+++ src/renderer/src/components/ResetAllDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <ConfirmDialog style="max-width: 75vw" />
+</template>
+
+<script setup>
+import { useConfirm } from 'primevue/useconfirm'
+
+const confirm = useConfirm()
+
+if (window.electronAPI !== undefined) {
+  window.electronAPI.onResetAll(() => {
+    confirm.require({
+      header: 'OpenCOR',
+      icon: 'pi pi-question-circle',
+      message: 'You are about to reset all of your settings. Do you want to proceed?',
+      acceptProps: {
+        severity: 'danger'
+      },
+      rejectProps: {
+        severity: 'secondary',
+        outlined: true
+      },
+      accept: () => {
+        window.electronAPI.resetAll()
+      }
+    })
+  })
+}
+</script>
