62e74ce5e7ea9af98579392bbfaada6dd23e734c
Alan Garny <agarny@hellix.com>
2024-10-14 14:39:01 +1300

    Splash screen: use TypeScript rather than JavaScript.
    Mainly for consistency in this case since we are doing nothing fancy.

--- src/main/assets/splashscreen.ts
+++ src/main/assets/splashscreen.ts
@@ -1,4 +1,7 @@
+// @ts-ignore (window.electronAPI is injected by Electron)
 window.electronAPI.onInitSplashScreenWindow((info) => {
+  // @ts-ignore(the copyright element exists)
   document.getElementById('copyright').innerText = info.copyright
+  // @ts-ignore(the version element exists)
   document.getElementById('version').innerText = info.version
 })
