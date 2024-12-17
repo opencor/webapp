0c06a3b881e72c2d2bde8f5482edbd9192a46604
Alan Garny <agarny@hellix.com>
2024-12-18 09:43:49 +1300

    Added a basic menu to the Web app.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <Background />
+  <div class="h-screen">
+    <div class="flex flex-col h-full">
+      <div v-if="electronAPI === undefined" class="main-menu">
+        <MainMenu @about="aboutVisible = true" />
+      </div>
+      <div class="flex grow justify-center items-center">
+        <Background />
+      </div>
+    </div>
+  </div>
   <ResetAllDialog />
-  <AboutDialog v-model:visible="visible" @close="visible = false" />
+  <AboutDialog v-model:visible="aboutVisible" @close="aboutVisible = false" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const visible = ref(false)
-
 // @ts-ignore (window.electronAPI may or not be defined and that is why we test it)
 const electronAPI = window.electronAPI
+const aboutVisible = ref(false)
 
 if (electronAPI !== undefined) {
   electronAPI.onAbout(() => {
-    visible.value = true
+    aboutVisible.value = true
   })
 }
 </script>
---
+++ src/renderer/src/components/MainMenu.vue
@@ -0,0 +1,53 @@
+<template>
+  <Menubar id="mainMenu" :model="items" />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const emit = defineEmits(['about'])
+
+const items = ref([
+  {
+    label: 'Help',
+    items: [
+      {
+        label: 'Home Page',
+        command: () => {
+          window.open('https://opencor.ws/')
+        }
+      },
+      { separator: true },
+      {
+        label: 'Report Issue',
+        command: () => {
+          window.open('https://github.com/opencor/webapp/issues/new')
+        }
+      },
+      { separator: true },
+      {
+        label: 'About OpenCOR',
+        command: () => {
+          emit('about')
+        }
+      }
+    ]
+  }
+])
+
+// Never display our menu as a hamburger menu.
+
+onMounted(() => {
+  const mainMenu = document.getElementById('mainMenu')
+
+  if (mainMenu !== null) {
+    const observer = new MutationObserver(() => {
+      if (mainMenu.className.includes('p-menubar-mobile')) {
+        mainMenu.classList.remove('p-menubar-mobile')
+      }
+    })
+
+    observer.observe(mainMenu, { attributes: true })
+  }
+})
+</script>
