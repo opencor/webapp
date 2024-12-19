c327bce54dd39009f21f9c6c46afe0158f34b44e
Alan Garny <agarny@hellix.com>
2024-12-19 16:30:48 +1300

    Go back to using PrimeIcons.
    Indeed, not sure how we can use FontAwesome icons in Menubar (see https://github.com/orgs/primefaces/discussions/3252).

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,7 +4,6 @@
       <div v-if="electronAPI === undefined" class="main-menu">
         <MainMenu @about="aboutVisible = true" />
       </div>
-      <FontAwesomeIcon icon="house" />
       <div class="flex grow justify-center items-center">
         <Background />
       </div>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,14 +1,10 @@
-import { library as faLibrary } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'primeicons/primeicons.css'
 import Aura from '@primevue/themes/aura'
 import PrimeVue from 'primevue/config'
 import ConfirmationService from 'primevue/confirmationservice'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/app.css'
-
-faLibrary.add(fas)
 
 const app = createApp(App)
 
@@ -18,7 +14,5 @@ app.use(PrimeVue, {
   }
 })
 app.use(ConfirmationService)
-
-app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.mount('#app')
