b9e760373539444d3b4d8df3efdcbcae0d37592c
Alan Garny <agarny@hellix.com>
2024-12-19 16:29:48 +1300

    Attempt at using FontAwesome icons.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,6 +4,7 @@
       <div v-if="electronAPI === undefined" class="main-menu">
         <MainMenu @about="aboutVisible = true" />
       </div>
+      <FontAwesomeIcon icon="house" />
       <div class="flex grow justify-center items-center">
         <Background />
       </div>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,10 +1,14 @@
-import 'primeicons/primeicons.css'
+import { library as faLibrary } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Aura from '@primevue/themes/aura'
 import PrimeVue from 'primevue/config'
 import ConfirmationService from 'primevue/confirmationservice'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/app.css'
+
+faLibrary.add(fas)
 
 const app = createApp(App)
 
@@ -14,5 +18,7 @@ app.use(PrimeVue, {
   }
 })
 app.use(ConfirmationService)
+
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.mount('#app')
