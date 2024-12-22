bc5599803238a7efaa2016f58b5818c92745a09b
Alan Garny <agarny@hellix.com>
2024-12-22 16:34:44 +1300

    libOpenCOR: trying to import a .node file from TypeScript.

--- src/libopencor/libOpenCOR.ts
+++ src/libopencor/libOpenCOR.ts
@@ -1,12 +1,5 @@
-// @ts-ignore (window.electronAPI may or not be defined and that is why we test it)
-const electronAPI = window.electronAPI
-
-// Export a function named ocVersion
+import * as oc from '../../out/libOpenCOR/Release/libOpenCOR.node'
 
 export function version() {
-  if (electronAPI !== undefined) {
-    return 'x.y.z'
-  } else {
-    return 'a.b.c'
-  }
+  return oc.version()
 }
