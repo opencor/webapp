45970cea456540f2a8899ee6f47f109148babaaa
Alan Garny <agarny@hellix.com>
2024-12-28 10:06:51 +1300

    Preload: removed unnecessary code.
    Indeed, `window.api` is currently empty and when it comes to `window.electron`, we don't make use of it since we set up some bridging below.

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,34 +1,15 @@
 import { contextBridge, ipcRenderer } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
-
-const api = {}
-
-// If context isolation is enabled then expose the Electron API to the renderer otherwise to the global DOM.
-
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  // @ts-ignore (window.electron is defined in lib.dom.d.ts)
-  window.electron = electronAPI
-  // @ts-ignore (window.api is defined in lib.dom.d.ts)
-  window.api = api
-}
 
 // Some bridging between our main process and renderer process.
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Handlers.
+  // Renderer process asking the main process to do something for it.
 
   resetAll: () => ipcRenderer.invoke('reset-all'),
   enableMenu: () => ipcRenderer.invoke('enable-menu'),
   disableMenu: () => ipcRenderer.invoke('disable-menu'),
 
-  // Callbacks.
+  // Renderer process listening to the main process.
 
   onInitSplashScreenWindow: (callback: (info: { message: string }) => void) =>
     ipcRenderer.on('init-splash-screen-window', (_event, info) => callback(info)),
