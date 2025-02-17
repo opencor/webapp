21955383f8658b7ded12c49ba3354330a2080363
Alan Garny <agarny@hellix.com>
2025-02-17 15:59:06 +1300

    Don't use browser-fs-access anymore.
    Not sure why, but it seemed slow to me. Now, we are just relying on the `input` HTML element, i.e. the old way of doing things. And, in that regards, we now retrieve a Vue reference to that HTML element.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="h-screen">
+  <div ref="app" class="h-screen">
     <div class="flex flex-col h-full">
       <div v-if="!electronAPI" class="main-menu">
         <MainMenu @about="aboutVisible = true" @open="open()" />
@@ -9,13 +9,12 @@
       </div>
     </div>
   </div>
-  <input id="file" type="file" style="display: none" />
+  <input ref="files" type="file" multiple style="display: none" />
   <ResetAllDialog />
   <AboutDialog v-model:visible="aboutVisible" @close="aboutVisible = false" />
 </template>
 
 <script setup lang="ts">
-import { fileOpen } from 'browser-fs-access'
 import * as vue from 'vue'
 
 import { electronAPI } from '../../electronAPI'
@@ -30,15 +29,25 @@ electronAPI?.onAbout(() => {
 
 // Open file(s) dialog.
 
+const filesInput = vue.useTemplateRef('files')
+
 function open() {
-  fileOpen([
-    {
-      multiple: true
-    }
-  ])
-    .then((files) => {
+  const filesInputElement = filesInput.value as HTMLInputElement
+
+  filesInputElement.click()
+}
+
+vue.onMounted(() => {
+  const filesInputElement = filesInput.value as HTMLInputElement
+
+  filesInputElement.onchange = (event: Event) => {
+    event.stopPropagation()
+    event.preventDefault()
+
+    const files = filesInputElement.files
+
+    if (files !== null) {
       for (const file of files) {
-        console.log(file)
         file
           .arrayBuffer()
           .then((arrayBuffer) => {
@@ -53,57 +62,35 @@ function open() {
             console.log('Failed to read file:', error)
           })
       }
-    })
-    .catch((error: unknown) => {
-      console.log('Failed to open file(s):', error)
-    })
-}
-
-const fileInput = document.getElementById('file') as HTMLInputElement | null
-
-if (fileInput !== null) {
-  fileInput.addEventListener('click', (event: Event) => {
-    event.stopPropagation()
-    event.preventDefault()
-
-    const files = fileInput.files
-
-    if (files !== null) {
-      for (const file of Array.from(files)) {
-        console.log('---------')
-        console.log(file.name)
-
-        if (electronAPI !== undefined) {
-          console.log(electronAPI.filePath(file))
-        }
-      }
     }
-  })
-}
+  }
+})
 
 // Handle drag and drop events.
 
-const dropArea = document.getElementById('app')
-let draggingCounter = 0
+const dropArea = vue.useTemplateRef('app')
 
-if (dropArea !== null) {
-  dropArea.addEventListener('dragenter', (event: Event) => {
+vue.onMounted(() => {
+  const dropAreaElement = dropArea.value as HTMLInputElement
+  let draggingCounter = 0
+
+  dropAreaElement.ondragenter = (event: DragEvent) => {
     event.stopPropagation()
     event.preventDefault()
 
     // Show the drop area, but only when entering it for the first time.
 
     if (draggingCounter++ === 0) {
-      dropArea.classList.add('dragging')
+      dropAreaElement.classList.add('dragging')
     }
-  })
+  }
 
-  dropArea.addEventListener('dragover', (event: Event) => {
+  dropAreaElement.ondragover = (event: DragEvent) => {
     event.stopPropagation()
     event.preventDefault()
-  })
+  }
 
-  dropArea.addEventListener('drop', (event: Event) => {
+  dropAreaElement.ondrop = (event: DragEvent) => {
     event.stopPropagation()
     event.preventDefault()
 
@@ -111,11 +98,11 @@ if (dropArea !== null) {
 
     draggingCounter = 0
 
-    dropArea.classList.remove('dragging')
+    dropAreaElement.classList.remove('dragging')
 
     // Handle the dropped files.
 
-    const files = (event as DragEvent).dataTransfer?.files
+    const files = event.dataTransfer?.files
 
     if (files !== undefined) {
       for (const file of Array.from(files)) {
@@ -127,17 +114,17 @@ if (dropArea !== null) {
         }
       }
     }
-  })
+  }
 
-  dropArea.addEventListener('dragleave', (event: Event) => {
+  dropAreaElement.ondragleave = (event: DragEvent) => {
     event.stopPropagation()
     event.preventDefault()
 
     // Hide the drop area, but only when leaving it for the last time.
 
     if (--draggingCounter === 0) {
-      dropArea.classList.remove('dragging')
+      dropAreaElement.classList.remove('dragging')
     }
-  })
-}
+  }
+})
 </script>
