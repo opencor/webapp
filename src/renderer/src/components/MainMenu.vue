5b50457e2a6bd4d903a4b8eb34b6ecf4f61371a8
Alan Garny <agarny@hellix.com>
2025-03-14 11:42:14 +1300

    MainMenu: some minor adjustments for the rendering of a shortcut.

--- src/renderer/src/components/MainMenu.vue
+++ src/renderer/src/components/MainMenu.vue
@@ -2,16 +2,16 @@
   <Menubar id="mainMenu" :model="items">
     <template #item="{ item, props }">
       <a v-bind="props.action">
-        <span class="p-menubar-item-label">{{ item.label }}</span>
+        <div class="p-menubar-item-label">{{ item.label }}</div>
         <svg v-if="item.items" width="14" height="14" viewBox="0 0 14 14" class="ml-auto p-icon p-menubar-submenu-icon">
           <path
             d="M5.25 11.1728C5.14929 11.1694 5.05033 11.1455 4.9592 11.1025C4.86806 11.0595 4.78666 10.9984 4.72 10.9228C4.57955 10.7822 4.50066 10.5916 4.50066 10.3928C4.50066 10.1941 4.57955 10.0035 4.72 9.86283L7.72 6.86283L4.72 3.86283C4.66067 3.71882 4.64765 3.55991 4.68275 3.40816C4.71785 3.25642 4.79932 3.11936 4.91585 3.01602C5.03238 2.91268 5.17819 2.84819 5.33305 2.83149C5.4879 2.81479 5.64411 2.84671 5.78 2.92283L9.28 6.42283C9.42045 6.56346 9.49934 6.75408 9.49934 6.95283C9.49934 7.15158 9.42045 7.34221 9.28 7.48283L5.78 10.9228C5.71333 10.9984 5.63193 11.0595 5.5408 11.1025C5.44966 11.1455 5.35071 11.1694 5.25 11.1728Z"
             fill="currentColor"
           />
         </svg>
-        <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-xs/3">{{
-          item.shortcut
-        }}</span>
+        <div v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-xs/3">
+          {{ item.shortcut }}
+        </div>
       </a>
     </template>
   </Menubar>
@@ -110,3 +110,11 @@ vueusecore.onKeyStroke((event) => {
   }
 })
 </script>
+
+<style scoped>
+.shortcut {
+  border-color: var(--p-content-border-color);
+  background: var(--p-content-hover-background);
+  color: var(--p-text-muted-color);
+}
+</style>
