240c9afc2693f853a3ff0375245e6421a11312cf
Alan Garny <agarny@hellix.com>
2025-03-26 18:06:06 +1300

    App: disable scrolling on the app itself.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col h-screen overflow-hidden">
     <div v-if="!electronAPI">
       <MainMenu
         :hasFiles="hasFilesRef"
