b552f56939f4a559bc988889a61acc353cda4300
Alan Garny <agarny@hellix.com>
2025-06-17 14:01:10 +1200

    Simulation Experiment view: use the small version of the Select component.

--- src/renderer/src/components/views/SimulationExperimentView.vue
+++ src/renderer/src/components/views/SimulationExperimentView.vue
@@ -22,6 +22,7 @@
                 filter
                 filterMode="lenient"
                 :options="parameters"
+                size="small"
                 class="w-full"
                 @change="updatePlot()"
               />
@@ -32,6 +33,7 @@
                 filter
                 filterMode="lenient"
                 :options="parameters"
+                size="small"
                 class="w-full"
                 @change="updatePlot()"
               />
