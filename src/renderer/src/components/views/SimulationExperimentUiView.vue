df880de32d062bcbb3cc9fdac8511470914aae8f
Alan Garny <agarny@hellix.com>
2025-06-23 14:40:44 +1200

    Simulation Experiment UI view: show/hide the input widgets, as needed.

--- src/renderer/src/components/views/SimulationExperimentUiView.vue
+++ src/renderer/src/components/views/SimulationExperimentUiView.vue
@@ -6,6 +6,7 @@
           <InputWidget
             v-for="(input, index) in (fileTabModel.uiJson as any).input"
             v-model="inputValues[index]"
+            v-show="showInput[index]"
             :key="'input_' + index"
             :name="input.name"
             :maximumValue="input.maximumValue"
@@ -13,7 +14,7 @@
             :possibleValues="input.possibleValues"
             :stepValue="input.stepValue"
             :class="index !== 0 ? 'mt-6' : ''"
-            @change="updateSimulation"
+            @change="updateUiAndSimulation"
           />
         </Fieldset>
       </div>
@@ -54,10 +55,27 @@ const plotsDivId = 'plotsDiv_' + String(fileTabModel.value.file.path())
 const plots = vue.ref<IGraphPanelPlot[][]>([])
 const issues = vue.ref(locApi.uiJsonIssues(fileTabModel.value.uiJson))
 const inputValues = vue.ref<number[]>([])
+const showInput = vue.ref<boolean[]>([])
 const idToInfo: Record<string, common.ISimulationDataInfo> = {}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function evaluateValue(value: string): any {
+  let index = -1
+  const parser = math.parser()
+
+  fileTabModel.value.uiJson?.input.forEach((input: locApi.IUiJsonInput) => {
+    parser.set(input.id, inputValues.value[++index])
+  })
+
+  return parser.evaluate(value)
+}
 
 fileTabModel.value.uiJson?.input.forEach((input: locApi.IUiJsonInput) => {
   inputValues.value.push(input.defaultValue)
+})
+
+fileTabModel.value.uiJson?.input.forEach((input: locApi.IUiJsonInput) => {
+  showInput.value.push(evaluateValue(input.visible ?? 'true'))
 })
 
 fileTabModel.value.uiJson?.output.data.forEach((data: locApi.IUiJsonOutputData) => {
@@ -65,7 +83,7 @@ fileTabModel.value.uiJson?.output.data.forEach((data: locApi.IUiJsonOutputData) 
 })
 
 vue.onMounted(() => {
-  updateSimulation()
+  updateUiAndSimulation()
 
   // Determine the number of graph panel widgets (needed to set their height).
 
@@ -74,26 +92,21 @@ vue.onMounted(() => {
   plotsDiv?.style.setProperty('--graph-panel-widget-count', plotsDiv.children.length.toString())
 })
 
-function updateSimulation() {
+function updateUiAndSimulation() {
+  // Show/hide the input widgets.
+
+  fileTabModel.value.uiJson?.input.forEach((input: locApi.IUiJsonInput, index: number) => {
+    showInput.value[index] = evaluateValue(input.visible ?? 'true')
+  })
+
   // Update the SED-ML document.
-
-  function evaluateValue(value: string): string {
-    let index = -1
-    const parser = math.parser()
-
-    fileTabModel.value.uiJson?.input.forEach((input: locApi.IUiJsonInput) => {
-      parser.set(input.id, inputValues.value[++index])
-    })
-
-    return parser.evaluate(value).toString()
-  }
 
   model.removeAllChanges()
 
   fileTabModel.value.uiJson?.parameters?.forEach((parameter: locApi.IUiJsonParameter) => {
     const componentVariableNames = parameter.name.split('/')
 
-    model.addChange(componentVariableNames[0], componentVariableNames[1], evaluateValue(parameter.value))
+    model.addChange(componentVariableNames[0], componentVariableNames[1], evaluateValue(parameter.value).toString())
   })
 
   // Run the instance and update the plots.
