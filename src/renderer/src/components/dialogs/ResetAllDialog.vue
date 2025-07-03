adc202a1a041ad4559412ec497425f011b578812
Alan Garny <agarny@hellix.com>
2025-07-03 12:21:07 +1200

    Dialogs: no need to specify the type of a `Button` object.

--- src/renderer/src/components/dialogs/ResetAllDialog.vue
+++ src/renderer/src/components/dialogs/ResetAllDialog.vue
@@ -4,8 +4,8 @@
       <label>You are about to reset all of your settings. Do you want to proceed?</label>
     </div>
     <template #footer>
-      <Button type="button" autofocus label="Yes" severity="danger" @click="$emit('resetAll')" />
-      <Button type="button" label="No" severity="secondary" @click="$emit('close')" />
+      <Button autofocus label="Yes" severity="danger" @click="$emit('resetAll')" />
+      <Button label="No" severity="secondary" @click="$emit('close')" />
     </template>
   </BaseDialog>
 </template>
