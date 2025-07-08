ec6ed9f7dd65f9fafe79213b358264176ab9f626
Alan Garny <agarny@hellix.com>
2025-07-09 11:26:47 +1200

    Tailwind CSS: some minor cleaning up.

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['src/renderer/index.html', 'src/renderer/src/**/*.{vue,ts}'],
-  theme: {
-    extend: {}
-  },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
-  plugins: [require('tailwindcss-primeui')]
-}
