cf42514153f9dbca377e3b1494bb717e888c5ab0
Alan Garny <agarny@hellix.com>
2025-07-09 13:13:49 +1200

    Property editor: slight improvements to the editing CSS.

--- src/renderer/src/components/propertyEditors/PropertyEditor.vue
+++ src/renderer/src/components/propertyEditors/PropertyEditor.vue
@@ -2,13 +2,6 @@
   <Fieldset :legend="name">
     <DataTable
       editMode="cell"
-      :pt="{
-        column: {
-          bodycell: ({ state }) => ({
-            class: [{ '!py-0': state['d_editing'] }]
-          })
-        }
-      }"
       resizableColumns
       showGridlines
       size="small"
@@ -58,5 +51,11 @@ function onCellEditComplete(event: DataTableCellEditCompleteEvent): void {
 <style scoped>
 :deep(.p-datatable-header-cell) {
   transition: none;
+}
+
+:deep(.p-inputnumber-input) {
+  padding: 0 2px !important;
+  margin: 0 !important;
+  line-height: 1.2px !important;
 }
 </style>
