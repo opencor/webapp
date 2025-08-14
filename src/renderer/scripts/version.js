1f4776ca16395ff44434651617800507c81da608
Alan Garny <agarny@hellix.com>
2025-08-14 19:31:53 +1200

    Have the patch version set automatically.

--- src/renderer/scripts/version.js
+++ src/renderer/scripts/version.js
@@ -1,14 +1,36 @@
+import { execSync } from 'child_process'
 import fs from 'fs'
 
-const versionMajor = 0
-const versionPatch = 0
+// Major version.
+
+const majorVersion = 0
+
+// Minor version.
 
 const now = new Date()
 const year = now.getFullYear()
 const month = now.getMonth() + 1
 const day = now.getDate()
+const minorVersion = `${year}${String(month).padStart(2, '0')}${String(day).padStart(2, '0')}`
 
-const version = `${versionMajor}.${year}${String(month).padStart(2, '0')}${String(day).padStart(2, '0')}.${versionPatch}`
+// Patch version.
+
+const gitDescribe = execSync('git describe --tags --long', { encoding: 'utf8' }).trim()
+const match = gitDescribe.match(/^v?(\d+)\.(\d+)\.(\d+)-(\d+)-g[0-9a-f]+$/)
+const [, , minor, patch] = match
+let patchVersion = parseInt(patch)
+
+if (minor === minorVersion) {
+  ++patchVersion
+} else {
+  patchVersion = 0
+}
+
+// (Full) version.
+
+const version = `${majorVersion}.${minorVersion}.${patchVersion}`
+
+// Update our package.json files.
 
 function updatePackageJsonFile(filePath) {
   const contents = JSON.parse(fs.readFileSync(filePath))
