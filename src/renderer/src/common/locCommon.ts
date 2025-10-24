d2146919653a4dcaa975c7b29cccaad2b9fe4f93
Alan Garny <agarny@hellix.com>
2025-10-25 12:14:46 +1300

    proxyUrl(): don't need to encode the URL.

--- src/renderer/src/common/locCommon.ts
+++ src/renderer/src/common/locCommon.ts
@@ -4,7 +4,7 @@ import * as locApi from '../libopencor/locApi.js';
 // Some file-related methods.
 
 export function proxyUrl(url: string): string {
-  return `https://cors-proxy.opencor.workers.dev/?url=${encodeURIComponent(url)}`;
+  return `https://cors-proxy.opencor.workers.dev/?url=${url}`;
 }
 
 export function isRemoteFilePath(filePath: string): boolean {
