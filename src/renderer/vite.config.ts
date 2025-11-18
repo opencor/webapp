f8e85feaceef687dadc27625da12c030db661e97
Alan Garny <agarny@hellix.com>
2025-11-18 18:03:54 +1300

    CD

--- src/renderer/vite.config.ts
+++ src/renderer/vite.config.ts
@@ -19,17 +19,6 @@ export default vite.defineConfig({
     },
     target: 'esnext'
   },
-  define: {
-    'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
-    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
-    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
-    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
-    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
-      process.env.VITE_FIREBASE_MESSAGING_SENDER_ID
-    ),
-    'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
-    'import.meta.env.VITE_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.VITE_FIREBASE_MEASUREMENT_ID)
-  },
   optimizeDeps: {
     esbuildOptions: {
       target: 'esnext'
