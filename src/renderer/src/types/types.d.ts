c6873e863c909509c52ea962303ae71dc4b57a52
Alan Garny <agarny@hellix.com>
2026-01-10 19:57:52 +1300

    Added a couple of packages and a type description.
    I get the feeling that they were already present for being needed by other packages, but we use them ourselves so we added them to prevent VS Code from (indirectly) complaining about not being able to find them.

--- src/renderer/src/types/types.d.ts
+++ src/renderer/src/types/types.d.ts
@@ -1,3 +1,11 @@
+declare module 'crypto-js/sha256' {
+  import type { WordArray } from 'crypto-js';
+
+  function SHA256(message: string | WordArray): WordArray;
+
+  export default SHA256;
+}
+
 declare module 'libopencor' {
   const libOpenCOR: () => Promise<unknown>;
 
