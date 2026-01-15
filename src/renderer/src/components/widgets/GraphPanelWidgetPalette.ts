e311a4f43b66d9b6df3e8f6848bee949cc3d44d2
Alan Garny <agarny@hellix.com>
2026-01-15 21:55:31 +1300

    Graph Panel widget: update the Tableau 10 palette of colours.
    See https://jrnold.github.io/ggthemes/reference/tableau_color_pal.html.

--- src/renderer/src/components/widgets/GraphPanelWidgetPalette.ts
+++ src/renderer/src/components/widgets/GraphPanelWidgetPalette.ts
@@ -1,11 +1,14 @@
 // Plotly color palette for graph panels.
 
 export const GraphPanelWidgetPalette = [
-  '#7289ab', // Blue
-  '#ea7e53', // Orange
-  '#eedd78', // Yellow
-  '#e69d87', // Pink
-  '#73a373', // Green
-  '#73b9bc', // Cyan
-  '#dd6b66' // Red
+  '#4e79a7', // Blue
+  '#f28e2b', // Orange
+  '#e15759', // Red
+  '#76b7b2', // Teal
+  '#59a14f', // Green
+  '#edc948', // Yellow
+  '#b07aa1', // Purple
+  '#ff9da7', // Pink
+  '#9c755f', // Brown
+  '#bab0ac' // Gray
 ];
