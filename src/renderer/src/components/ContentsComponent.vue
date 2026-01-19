8b6bff5bdc942812b8aee5df8c56d2d261f584e7
Alan Garny <agarny@hellix.com>
2026-01-19 13:58:01 +1300

    Contents component: make sure that a file tab doesn't have any URL encoding.

--- src/renderer/src/components/ContentsComponent.vue
+++ src/renderer/src/components/ContentsComponent.vue
@@ -39,10 +39,7 @@
           <div class="flex gap-2 items-center">
             <div>
               {{
-                fileTab.file
-                  .path()
-                  .split(/(\\|\/)/g)
-                  .pop()
+                fileName(fileTab.file.path())
               }}
             </div>
             <div class="pi pi-times remove-button" @mousedown.prevent @click.stop="closeFile(fileTab.file.path())" />
@@ -135,6 +132,18 @@ vue.watch(activeFile, (newActiveFile: string) => {
 
   electronApi?.fileSelected(newActiveFile);
 });
+
+function fileName(filePath: string): string {
+  const res = filePath.split(/(\\|\/)/g).pop() || '';
+
+  try {
+    return decodeURIComponent(res);
+  } catch (error: unknown) {
+    console.error('Failed to decode the file path:', res, error);
+
+    return res;
+  }
+}
 
 function openFile(file: locApi.File): void {
   const filePath = file.path();
