cb8c5a677dc3110347207592ccfc30de5d2de87a
Alan Garny <agarny@hellix.com>
2026-01-29 21:19:40 +1300

    Contents component: moved our `fileName()` method to `common.ts`.

--- src/renderer/src/common/common.ts
+++ src/renderer/src/common/common.ts
@@ -144,3 +144,17 @@ export function downloadFile(filename: string, content: string | Blob, type: str
 
   URL.revokeObjectURL(url);
 }
+
+// A method to get the file name from a given file path.
+
+export function fileName(filePath: string): string {
+  const res = filePath.split(/(\\|\/)/g).pop() || '';
+
+  try {
+    return decodeURIComponent(res);
+  } catch (error: unknown) {
+    console.error('Failed to decode the file path:', res, error);
+
+    return res;
+  }
+}
