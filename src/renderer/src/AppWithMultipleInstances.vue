0e26e326bdf3ca10d9890ebb65d0e9de7f149de1
Alan Garny <agarny@hellix.com>
2026-02-14 19:51:32 +1300

    Updated our main.ts and App.vue.
    App.vue used to contained various testing code. So, it is a bit cleaner now.

--- src/renderer/src/AppWithMultipleInstances.vue
+++ src/renderer/src/AppWithMultipleInstances.vue
@@ -29,3 +29,7 @@
     </Splitter>
   </div>
 </template>
+
+<style>
+  @import "tailwindcss/preflight.css" layer(base);
+</style>
--- src/renderer/src/AppWithinContainers.vue
+++ src/renderer/src/AppWithinContainers.vue
@@ -7,10 +7,6 @@
           <SplitterPanel class="flex items-center justify-center bg-red-500" :size="15">Left panel...</SplitterPanel>
           <SplitterPanel :size="50">
             <OpenCOR />
-            <!-- <OpenCOR omex="https://raw.githubusercontent.com/opencor/webapp/refs/heads/main/tests/models/lorenz.cellml" /> -->
-            <!-- <OpenCOR omex="https://raw.githubusercontent.com/opencor/webapp/refs/heads/main/tests/models/lorenz.omex" /> -->
-            <!-- <OpenCOR omex="https://raw.githubusercontent.com/opencor/webapp/refs/heads/main/tests/models/ui/lorenz.omex" /> -->
-            <!-- <OpenCOR omex="https://models.physiomeproject.org/workspace/b7c/rawfile/e0ae8d2d56aaaa091e23e1ee7e84cacbda1dfb6b/lorenz.omex" /> -->
           </SplitterPanel>
           <SplitterPanel class="flex items-center justify-center bg-red-500" :size="35">Right panel...</SplitterPanel>
         </Splitter>
@@ -19,3 +15,7 @@
     </Splitter>
   </div>
 </template>
+
+<style>
+  @import "tailwindcss/preflight.css" layer(base);
+</style>
