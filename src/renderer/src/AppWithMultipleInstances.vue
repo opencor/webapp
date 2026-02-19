0a9e0a10a81003cdad9c7184619108d723f163ea
Alan Garny <agarny@hellix.com>
2026-02-20 01:47:27 +1300

    App with multiple instances: some minor cleaning up.

--- src/renderer/src/AppWithMultipleInstances.vue
+++ src/renderer/src/AppWithMultipleInstances.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen">
+  <div class="h-screen overflow-hidden">
     <Splitter class="h-full" layout="vertical">
       <SplitterPanel :size="15">
         <OpenCOR />
