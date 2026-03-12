835882c6015d7f19a67b69ede8fbc110a446e386
Alan Garny <agarny@hellix.com>
2026-03-12 16:19:33 +1300

    OpenCOR: slight improvements to our dropping zone.

--- src/renderer/src/components/DragNDropComponent.vue
+++ src/renderer/src/components/DragNDropComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute w-full h-full border-[0.375rem] border-dashed border-primary z-99999">
+  <div class="absolute w-full h-full border-[0.375rem] border-dashed border-primary z-99999 highlight">
     <div
       class="message absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[1.5rem] leading-tight text-center rounded-2xl p-[0.5rem_1rem] bg-primary text-primary-contrast whitespace-nowrap"
     >
@@ -10,6 +10,10 @@
 </template>
 
 <style scoped>
+.highlight {
+  background-color: color-mix(in srgb, var(--p-primary-color) 7%, transparent);
+}
+
 .message {
   box-shadow: 0 0 0.75rem 0.375rem var(--p-content-border-color);
 }
